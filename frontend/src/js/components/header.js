0f5b6ae3ac208410ea727812f7402bebad2516d0
Chuck R <rapmastercm@aol.com>
2020-07-09 11:00:10 -0400

    tested header function

--- frontend/src/js/components/header.js
+++ frontend/src/js/components/header.js
@@ -2,8 +2,9 @@ export default function Header() {
     return `
       <nav class='nav__header'>
       <ul>
-        <li class='nav__home'>Home</li>
-        <li class='nav__todos'>ToDos</li>
+        <li class='nav__home'>Artist</li>
+        <li class='nav__todos'>Albums</li>
+        <li>Text here </li>
       </ul>
       </nav>  
       `;
