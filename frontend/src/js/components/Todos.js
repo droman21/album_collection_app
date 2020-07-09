937b0e323e7959339b36936c7e2249f92f0ba9ac
Chuck R <rapmastercm@aol.com>
2020-07-09 10:53:18 -0400

    updated frontend

---
+++ frontend/src/js/components/Todos.js
@@ -0,0 +1,22 @@
+export default function Todos(todos){
+    console.log(`in the todo component ${todos}`)
+    return `
+    <ol> 
+        ${todos.map(todo => {
+            return `
+            <li>
+                <h4 class="todo__items">${todo.name} by ${todo.owner}</h4>
+                <button class="edit-todo__edit">Edit</button>
+                <button class="delete-todo__submit">Delete</button>
+                <input class="todo__id" type="hidden" value="${todo.id}">
+            </li>
+            `
+        }).join("")}
+    </ol>
+
+    <section class="add-todo">
+            <input class="add-todo__todoName" type="text" placeholder="Add a Todo here">
+            <button class="add-todo__submit">Add a Todo</button>
+    </section>
+    `
+}
---
+++ frontend/src/js/components/header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+    return `
+      <nav class='nav__header'>
+      <ul>
+        <li class='nav__home'>Home</li>
+        <li class='nav__todos'>ToDos</li>
+      </ul>
+      </nav>  
+      `;
+  }
