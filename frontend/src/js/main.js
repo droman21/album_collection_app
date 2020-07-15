76410d646bb397558a1ad9952da99d318e37e2e2
Corey T Cox <coxcorey19@gmail.com>
2020-07-15 15:26:18 -0400

    slight update

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -5,78 +5,81 @@ import apiActions from './api/apiActions';
 import Artists from './components/Artists';
 import Albums from './components/Albums';
 import Album from './components/Album';
+import Artist from './components/Artist';
 
 const appDiv = document.querySelector('.app');
 
 //pageBuild();
 
 export default function pageBuild() {
-    header();
-    //footer();
-    navHome();
-    showAlbumsByArtistID();
-    buildNav();
-    showArtists();
-    showAlbums();
-    albumLink();
-    showArtistsPageLoad();
+  header();
+  //footer();
+  navHome();
+  showAlbumsByArtistID();
+  // albumsByArtistIDLink();
+  buildNav();
+  showArtists();
+  showAlbums();
+  albumLink();
+  showArtistsPageLoad();
 }
 
 function header() {
-    const header = document.querySelector("#header");
-    header.innerHTML = Header(); 
+  const header = document.querySelector("#header");
+  header.innerHTML = Header();
 }
 
 function navHome() {
   const homeButton = document.querySelector(".nav__artist");
-  homeButton.addEventListener("click", function() {
+  homeButton.addEventListener("click", function () {
     document.querySelector("#app").innerHTML = Home();
   });
 }
 
-function showArtists(){
+function showArtists() {
   const artistLink = document.querySelector(".nav__artist");
-  artistLink.addEventListener('click', function(){
-        fetch("https://localhost:44313/api/artist")
-        .then(response => response.json())
-        .then(artists => {
-          appDiv.innerHTML = Artists(artists);
-          console.log(artists);
-        })
+  artistLink.addEventListener('click', function () {
+    fetch("https://localhost:44313/api/artist")
+      .then(response => response.json())
+      .then(artists => {
+        appDiv.innerHTML = Artists(artists);
+        console.log(artists);
       })
-}
-function showArtistsPageLoad(){
-        fetch("https://localhost:44313/api/artist")
-        .then(response => response.json())
-        .then(artists => {
-          appDiv.innerHTML = Artists(artists);
-          console.log(artists);
-        })
+  })
 }
 
-function showAlbums(){
-  const albumLink = document.querySelector('.nav__albums');
-  albumLink.addEventListener('click', function(){
-    fetch('https://localhost:44313/api/album')
+function showArtistsPageLoad() {
+  fetch("https://localhost:44313/api/artist")
     .then(response => response.json())
-    .then(albums => {
-      appDiv.innerHTML = Albums(albums)
-      albumLink();
+    .then(artists => {
+      appDiv.innerHTML = Artists(artists);
+      console.log(artists);
     })
-    .catch(err => console.log(err))
+}
+
+function showAlbums() {
+  const albumLink = document.querySelector('.nav__albums');
+  albumLink.addEventListener('click', function () {
+    fetch('https://localhost:44313/api/album')
+      .then(response => response.json())
+      .then(albums => {
+        appDiv.innerHTML = Albums(albums)
+        albumLink();
+      })
+      .catch(err => console.log(err))
   })
 }
 
 function albumLink() {
   const albumNameElements = document.querySelectorAll('.album__name');
   albumNameElements.forEach(element => {
-    element.addEventListener('click', function(){
+    element.addEventListener('click', function () {
       const albumId = element.id;
-      console.log("Album ID is"+albumId);
+      console.log("Album ID is" + albumId);
       fetch(`https://localhost:44313/api/album/${albumId}`)
-      .then(response => response.json())
-      .then(album => appDiv.innerHTML = Album(album))
-      .catch(err => console.log(err))
+        .then(response => response.json())
+        .then(album => appDiv.innerHTML = Album(album))
+        .catch(err => console.log(err))
     })
   })
 }
@@ -87,85 +90,99 @@ function showAlbumsByArtistID() {
     element.addEventListener('click', function() {
       const artistId = element.id;
       console.log("Artist ID is"+artistId);
-      fetch(`https://localhost:44313/api/album/${artistID}`)
+      fetch(`https://localhost:44313/api/album/${artistId}`)
       .then(response => response.json())
-      .then(albums => appDiv.innerHTML = Albums(albums))
+      .then(Artist => appDiv.innerHTML = Artist(artist))
+      // albumsByArtistIDLink();
       .catch(err => console.log(err))
     })
   })
 
 }
 
+// function albumsByArtistIDLink() {
+//   const albumNameElements = document.querySelectorAll('.artist__name');
+//   albumNameElements.forEach(element => {
+//     element.addEventListener('click', function () {
+//       const albumId = element.id;
+//       console.log("Album ID is" + albumId);
+//       fetch(`https://localhost:44313/api/album/${albumId}`)
+//         .then(response => response.json())
+//         .then(album => appDiv.innerHTML = Album(album))
+//         .catch(err => console.log(err))
+//     })
+//   })
+// }
+
 function buildNav() {
-    const artistButton = document.querySelector(".nav__artist");
-    const app = document.querySelector('#app');
+  const artistButton = document.querySelector(".nav__artist");
+  const app = document.querySelector('#app');
 
-    artistButton.addEventListener("click", function() {
-        fetch("https://localhost:44313/api/artist")
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .then(err => console.log(err))
-        .catch(err => console.log(err))
+  artistButton.addEventListener("click", function () {
+    fetch("https://localhost:44313/api/artist")
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .then(err => console.log(err))
+      .catch(err => console.log(err))
 
-      });
-    
-    // app.addEventListener("click", function(){
-    //   if(event.target.classList.contains('delete-todo__submit')){
-    //     const todoId = event.target.parentElement.querySelector('.todo__id').value;
-    //     console.log(todoId);
+  });
 
-    //     apiActions.deleteRequest(
-    //       `https://localhost:44393/api/todo/${todoId}`,
-    //       toDos => {
-    //         app.innerHTML = artists(toDos);
-    //       }
-    //     )
-    //   }
-    // })
+  // app.addEventListener("click", function(){
+  //   if(event.target.classList.contains('delete-todo__submit')){
+  //     const todoId = event.target.parentElement.querySelector('.todo__id').value;
+  //     console.log(todoId);
 
-    // app.addEventListener("click", function(){
-    //   if(event.target.classList.contains('edit-todo__edit')){
-    //     const todoId = event.target.parentElement.querySelector('.todo__id').value;
-    //     console.log(todoId);
+  //     apiActions.deleteRequest(
+  //       `https://localhost:44393/api/todo/${todoId}`,
+  //       toDos => {
+  //         app.innerHTML = artists(toDos);
+  //       }
+  //     )
+  //   }
+  // })
 
-    //     apiActions.getRequest(
-    //       `https://localhost:44393/api/artist/${artistId}`,
-    //       ArtistEdit => {
-    //         console.log(artistEdit);
-    //         app.innerHTML = ArtistEdit(artistEdit);
-    //       }
-    //     )
-    //   }
-    // })
+  // app.addEventListener("click", function(){
+  //   if(event.target.classList.contains('edit-todo__edit')){
+  //     const todoId = event.target.parentElement.querySelector('.todo__id').value;
+  //     console.log(todoId);
 
-    // app.addEventListener("click", function(){
-    //   if(event.target.classList.contains('update-todo__submit')){
-    //     const todoId = event.target.parentElement.querySelector('.update-todo__id').value;
-    //     const todoName = event.target.parentElement.querySelector('.update-todo__name').value;
-    //     const todoOwner = event.target.parentElement.querySelector('.update-todo__owner').value;
+  //     apiActions.getRequest(
+  //       `https://localhost:44393/api/artist/${artistId}`,
+  //       ArtistEdit => {
+  //         console.log(artistEdit);
+  //         app.innerHTML = ArtistEdit(artistEdit);
+  //       }
+  //     )
+  //   }
+  // })
 
-    //     console.log(todoId);
+  // app.addEventListener("click", function(){
+  //   if(event.target.classList.contains('update-todo__submit')){
+  //     const todoId = event.target.parentElement.querySelector('.update-todo__id').value;
+  //     const todoName = event.target.parentElement.querySelector('.update-todo__name').value;
+  //     const todoOwner = event.target.parentElement.querySelector('.update-todo__owner').value;
 
-    //     const todoData = {
-    //       id: todoId,
-    //       name: todoName,
-    //       owner: todoOwner
-    //     };
+  //     console.log(todoId);
 
-    //     console.log(todoData);
-    //     console.log(JSON.stringify(todoData));
+  //     const todoData = {
+  //       id: todoId,
+  //       name: todoName,
+  //       owner: todoOwner
+  //     };
+
+  //     console.log(todoData);
+  //     console.log(JSON.stringify(todoData));
 
 
-    //     apiActions.putRequest(
-    //       `https://localhost:44393/api/todo/${todoId}`,
-    //       todoData,
-    //       todos => {
-    //         console.log(todos);
-    //         app.innerHTML = Artists(todos);
-    //       }
-    //     );
-    //   }
+  //     apiActions.putRequest(
+  //       `https://localhost:44393/api/todo/${todoId}`,
+  //       todoData,
+  //       todos => {
+  //         console.log(todos);
+  //         app.innerHTML = Artists(todos);
+  //       }
+  //     );
+  //   }
 
-    }
-  
-  
+}
+
