9b0535f36b4653e02091934e8da2ae3a9b409828
Corey T Cox <coxcorey19@gmail.com>
2020-07-22 14:39:29 -0400

    NewStyling2 changes

---
+++ backend/album-collection/Migrations/20200722145928_UpdatedSeedData.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace album_collection.Migrations
+{
+    public partial class UpdatedSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "Image",
+                value: "HotelDiablo.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "Image",
+                value: "EatOrGetAte.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "Image",
+                value: "Lemonade.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "Image",
+                value: "MGK.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "Image",
+                value: "BigMoochie.jpg");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "Image",
+                value: "MGK.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "Image",
+                value: "BigMoochie.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Albums",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "Image",
+                value: "Lemonade. jpg");
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "Image",
+                value: "Image");
+
+            migrationBuilder.UpdateData(
+                table: "Artists",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "Image",
+                value: "Image");
+        }
+    }
+}
--- frontend/src/js/components/album.js
+++ frontend/src/js/components/album.js
@@ -1,12 +1,14 @@
 export default function album(album){
     return `
+        <section class="header-container">
         <h1>${album.title}</h1>
+        </section>
         <ol>
             ${album.songs.map(song =>{
                 return `
                 <li>
                     <h4>${song.name}</h4>
-                </li>
+                <li>
                 `
             }).join("")}
         <ol>
